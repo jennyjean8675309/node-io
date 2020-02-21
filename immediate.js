beba5fe96cfcb646c6c9e9f227803c5eca389577
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2020-02-21 11:18:42 -0500

    added some comments (as notes) to the files

--- immediate.js
+++ immediate.js
@@ -1,8 +1,17 @@
+// this very poorly name method, setImmediate(), 
+
+// runs first
 console.log('step 1')
+
+// gets put into the async queue
 setTimeout(function(){
   console.log('step 2')
 }, 0)
+
+// waits until AFTER the async queue is empty, then runs
 setImmediate(function(){
   console.log('step 2*')
 })
+
+// runs second
 console.log('step 1.5?')
--- nexttick.js
+++ nexttick.js
@@ -1,8 +1,12 @@
 console.log('step 1')
+
 setTimeout(function(){
   console.log('step 2')
 }, 0)
+
+// nextTick() is like an in-between async function - first, step 1 is logged, setTimeout gets put in the async queue to run later, step 1.5 is logged, AND THEN BEFORE OUR PROGRAM GOES BACK TO CALL THE SETTIMEOUT FUNCTION FROM THE ASYNC QUEUE NEXT TICK RUNS AND step 2* IS LOGGED TO THE CONSOLE, then we go to the async queue and setTimeout logs step 2
 process.nextTick(function(){
   console.log('step 2*')
 })
+
 console.log('step 1.5?')
